216458149b55932ce365abd97741f306e73cef48
Prabhnoor singh <141825545+prabhnoorsinngh@users.noreply.github.com>
2026-07-22 01:53:01 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (35.81%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,8 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        seen={}#kali dictionary(isme mai hum valu dale gee)
         for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return []# agr kabhi na mile (yahan prectically nhi ho)
+            complement = target - nums[i]#ek hi loop ek num pe khade hoo kar socho (agar nums =[2,7,11,15]hai aur target = 9 hai , aur hume i =0 pe hai num [0]=2 to joo complement = 9 - 2 =7) {matlab = muje 7 chiye taa ki array m,ai 2+7 = 9 bane }
+            if complement in seen:
+                return [seen[complement], i]#chack karne ke liye humne if statement kaa use kiya ki kya yee complement pele dekha tha
+            seen[nums[i]] = i
